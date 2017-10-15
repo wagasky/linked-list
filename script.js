f309dc18e716a4a360ffe25ff3250dfec0a5726b
James Logue <logueja@gmail.com>
2017-10-14 18:37:53 -0600

    Initial jQuery functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+var inputButton = document.querySelector("#input-button");
+var titleInput = $("#title-input");
+var urlInput = $("#url-input");
+var deleteButton = $(".link-card-delete-button")
+var listContainer = $("#list-container")
+
+var cardTitle
+var cardUrl
+
+inputButton.addEventListener('click', createCard)
+
+function createCard() {
+  console.log('click')
+  cardTitle = titleInput.val();
+  cardUrl = urlInput.val();
+
+  var timeId = Date.now();
+
+  listContainer.prepend(
+    '<article class="link-card-container" id=' + "'" + timeId + "'" + '> <h2 class="link-card-title">'+ cardTitle + '</h2><p>' + cardUrl + '</p> <footer class="link-card-footer"> <button class="link-card-read-button card-button">Read</button> <button class="link-card-delete-button card-button">Delete</button> </footer> </article>'
+  );
+
+  var deleteButton = document.querySelector('.link-card-delete-button')
+  var readButton = document.querySelector('.link-card-read-button')
+
+  deleteButton.addEventListener('click', function() {
+    $('#'+timeId).remove()
+  });
+
+  readButton.addEventListener('click', function() {
+    if( $('#'+timeId).hasClass("read") ) {
+      $('#'+timeId).removeClass("read")
+    } else {
+      $('#'+timeId).addClass('read')
+    }
+    console.log('classclick')
+  });
+
+};
+
