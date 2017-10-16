e509e3df761b6d947eda49270ade33549f41c11c
Melissa Wagasky <melissawagasky@gmail.com>
2017-10-15 19:06:19 -0600

    Add jquery script link to HTML file

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initial pass JS
+// branch to work on javascript
+// setting up variables/selectors
+// event listener for gather input field data
+// event listener for click on submit (related to above)
+// pseudo code for how we want to build the cards
+// Build the card
+// We have a unique for each card and an external counter so that time you create a card, we keep track of how many cards we have and so we can give each a unique identifier
+// Make test doc of links/titles
+// enter console logs to test
+
